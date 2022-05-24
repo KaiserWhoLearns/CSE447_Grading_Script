a1e619ac46d65295dbe02f01a58f753a34ec3c02
Kaiser <kaisersunhk@gmail.com>
2022-05-24 00:58:02 -0700

    Added batch grading

--- grade_in_batch.sh
+++ grade_in_batch.sh
@@ -3,5 +3,5 @@
 read -p "Enter the path to student netIds: " student_list_path
 while read line; do
     echo "Grading student $line..."
-    bash grade_a1.sh $line n
+    bash grade_a2.sh $line n
 done <$student_list_path
